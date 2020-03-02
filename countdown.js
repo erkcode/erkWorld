0ee893836cc30797c758ba00c9f9cede86239554
Erk <hello@erk.world>
2020-03-02 18:14:25 +0100

    changed launch date to March 21st

--- countdown.js
+++ countdown.js
@@ -1,21 +1,21 @@
-const launchDate = new Date('Mar 1, 2020 06:00:00').getTime();
+const launchDate = new Date('Mar 21, 2020 06:00:00').getTime()
 
-const second = 1000;
-const minute = second * 60;
-const hour = minute * 60;
-const day = hour * 24;
+const second = 1000
+const minute = second * 60
+const hour = minute * 60
+const day = hour * 24
 
 function countdown() {
-  const now = new Date().getTime();
-  const delta = launchDate - now;
-  const days = Math.floor(delta / day);
-  const hours = Math.floor((delta % day) / hour);
-  const minutes = Math.floor((delta % hour) / minute);
-  const seconds = Math.floor((delta % minute) / second);
-  document.getElementById('days').innerText = days;
-  document.getElementById('hours').innerText = hours;
-  document.getElementById('minutes').innerText = minutes;
-  document.getElementById('seconds').innerText = seconds;
+  const now = new Date().getTime()
+  const delta = launchDate - now
+  const days = Math.floor(delta / day)
+  const hours = Math.floor((delta % day) / hour)
+  const minutes = Math.floor((delta % hour) / minute)
+  const seconds = Math.floor((delta % minute) / second)
+  document.getElementById('days').innerText = days
+  document.getElementById('hours').innerText = hours
+  document.getElementById('minutes').innerText = minutes
+  document.getElementById('seconds').innerText = seconds
 }
 
-setInterval(countdown, 1000);
+setInterval(countdown, 1000)
